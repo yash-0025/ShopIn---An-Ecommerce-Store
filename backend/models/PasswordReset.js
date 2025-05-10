139ec7e982619c5bec58c30ddb84e531aa79fad5
yash-0025 <yashpatel200403@gmail.com>
2025-05-10 20:03:03 +0530

    # BACKEND :: OTP and TokenPasswordREset  SCHEMA CREATED

---
+++ backend/models/PasswordReset.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+const {Schema} = mongoose
+
+const passwordResetSchema = new Schema({
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    token:{
+        type:String,
+        required:true,
+    },
+    expiresAt: {
+        type: Date,
+        required: true
+    }
+})
+
+
+module.exports = mongoose.model("PasswordReset",passwordResetSchema)
