216eeada9d81baf5326c946499c80808cbe0ac73
yash-0025 <yashpatel200403@gmail.com>
2025-05-10 14:44:24 +0530

    # BACKEND :: CATEGORY SCHEMA CREATED

---
+++ backend/models/Category.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+const {Schema} = mongoose
+
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    }
+})
+
+module.exports = mongoose.model("Category",categorySchema);
