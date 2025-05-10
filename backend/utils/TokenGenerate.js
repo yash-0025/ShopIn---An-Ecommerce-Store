38784473c89b185867069a84415f845e11372282
yash-0025 <yashpatel200403@gmail.com>
2025-05-10 23:37:23 +0530

    # BACKEND :: USER CONTROLLER CREATED

--- backend/utils/TokenGenerate.js
+++ backend/utils/TokenGenerate.js
@@ -6,6 +6,6 @@ const PasswordReset = require("../models/PasswordReset")
 
 exports.generateToken = (payload, PasswordReset=false) => {
     return jwt.sign(payload, process.env.TOKEN_SECRET),{
-        expiresIn: PasswordReset?process.env.PASSWORD_RESET_TOKEN_EXPIRATION:process.env.LOGIN_TOKEN_EXPIRATION
+        expiresIn: PasswordReset ? process.env.PASSWORD_RESET_TOKEN_EXPIRATION:process.env.LOGIN_TOKEN_EXPIRATION
     }
 }
