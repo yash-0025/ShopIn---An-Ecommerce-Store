e145224e495c449cf407668ec3c3216308e1f260
yash-0025 <yashpatel200403@gmail.com>
2025-05-12 02:12:18 +0530

    # BACKEND :: BRAND CONTROLLER CREATED

---
+++ backend/controllers/Brand.js
@@ -0,0 +1,13 @@
+const Brand = require("../models/Brand")
+
+exports.getAllBrands = async(req,res) => {
+    try {
+        const result = await Brand.find({})
+        res.status(200).json(result)
+    } catch(error){
+        console.log(error)
+        res.status(500).json({
+            message:"Error fetching brands"
+        })
+    }
+}
