57bf7ce5bc90b07f4c8121d1e1f8a2272036cfe5
yash-0025 <yashpatel200403@gmail.com>
2025-05-11 22:43:03 +0530

    # BACKEND :: AUTH ROUTES UPDATED

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -5,5 +5,11 @@ const {verifyToken} = require("../middleware/TokenVerification")
 
 router.post("/signup", authController.signup);
 router.post("/login",authController.login);
+router.post("/verify-otp",authController.verifyOtp)
+router.post("/resend-otp",authController.resendOtp)
+router.post("/forgot-password", authController.forgotPassword)
+router.post("/reset-password",authController.resetPassword)
+router.get("/check-auth", verifyToken, authController.checAuth)
+router.get("/logout",authController.logout)
 
 module.exports = router
